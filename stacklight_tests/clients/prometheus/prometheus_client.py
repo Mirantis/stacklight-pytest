e8f673ef478a33aa313c8f6e15711dbfb34b194e
vitalygusev <vgusev@mirantis.com>
2019-02-28 16:00:59 +0400

    Use Prometheus LTS in tests by default
    Change-Id: Ie1a14ce2a705504bdc746f8e59bea19e13578d71
    Related-PROD: PROD-27747 (PROD:27747)

--- stacklight_tests/clients/prometheus/prometheus_client.py
+++ stacklight_tests/clients/prometheus/prometheus_client.py
@@ -152,9 +152,9 @@ class PrometheusClient(http_client.HttpClient):
 
 
 def get_prometheus_client_from_config(config):
-    api_client = PrometheusClient(
-        "http://{0}:{1}/".format(
-            config["prometheus_vip"],
-            config["prometheus_server_port"])
-    )
+    host = config["prometheus_relay_vip"] if config.get(
+        "use_prometheus_lts", False) else config["prometheus_vip"]
+    port = config["prometheus_relay_port"] if config.get(
+        "use_prometheus_lts", False) else config["prometheus_server_port"]
+    api_client = PrometheusClient("http://{0}:{1}/".format(host, port))
     return api_client
