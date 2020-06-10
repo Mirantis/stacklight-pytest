51c7ee0ab64ff3e66ff7799869533807308ccd54
Ilia Pchelov <ipchelov@mirantis.com>
2020-06-10 14:55:39 +0400

    Refactor test 'test_alerta_alerts_consistency'
    Related-PROD: PRODX-3930
    Change-Id: Ic5cf01f563e235945646a7ef9db8b40dfb46c9e5

--- stacklight_tests/tests/test_alerta.py
+++ stacklight_tests/tests/test_alerta.py
@@ -1,5 +1,6 @@
 import logging
 import pytest
+import datetime
 
 from stacklight_tests import utils
 
@@ -16,17 +17,21 @@ def test_alerta_smoke(alerta_api):
 @pytest.mark.alerta
 @pytest.mark.smoke
 @pytest.mark.run(order=-1)
-@pytest.mark.skip("Temporary skip, need to refactor")
 def test_alerta_alerts_consistency(prometheus_native_alerting, alerta_api):
-    def check_alerts():
+    def check_alerts(alert_age=300):
+        time_now = datetime.datetime.now()
         alerta_alerts = {"{0} {1}".format(i['event'], i['resource']).replace(
             "n/a", "") for i in alerta_api.get_alerts({"status": "open"})}
         alertmanager_alerts = {
             "{0} {1}".format(i.name, i.instance)
-            for i in prometheus_native_alerting.list_alerts()}
+            for i in prometheus_native_alerting.list_alerts()
+            if (prometheus_native_alerting.get and
+                utils.difference_in_seconds(utils.convert_unicode_to_datetime(
+                    list(i.time)[0]), time_now, alert_age))}
+        logger.info(list(prometheus_native_alerting.list_alerts()[0].time)[0])
         print alerta_alerts
         print alertmanager_alerts
-        if alerta_alerts == alertmanager_alerts:
+        if alertmanager_alerts.issubset(alerta_alerts):
             return True
         else:
             logger.warning(
@@ -36,7 +41,7 @@ def test_alerta_alerts_consistency(prometheus_native_alerting, alerta_api):
                     alertmanager_alerts.difference(alerta_alerts)))
             return False
 
-    utils.wait(check_alerts, interval=20, timeout=60,
+    utils.wait(check_alerts, interval=30, timeout=6 * 60,
                timeout_msg="Alerts in Alertmanager and Alerta incosistent")
 
 
--- stacklight_tests/utils.py
+++ stacklight_tests/utils.py
@@ -140,3 +140,16 @@ def skip_test(related_release, releases):
     if related_release not in releases:
         pytest.skip("Related release '{}' is not found in Stacklight CRD"
                     .format(related_release))
+
+
+def difference_in_seconds(date_one, date_two, difference_in_seconds):
+    # The function compares two dates and returns 'True' or 'False'
+    # depending on whether the difference more or less than
+    # parameter 'difference_in_seconds'
+    result = (date_two - date_one).total_seconds() > difference_in_seconds
+    return result
+
+
+def convert_unicode_to_datetime(unicode_date):
+    return dt.datetime.strptime(unicode_date[:-4],
+                                '%Y-%m-%dT%H:%M:%S.%f')
